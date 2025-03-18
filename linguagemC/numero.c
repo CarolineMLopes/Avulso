e9b7f486802bffd0107bc818be6b6f51b6f5db18
Caroline Marcon Lopes <carolinemarconlopes@gmail.com>
2025-03-18 10:36:01 -0300

    faculdade

---
+++ linguagemC/numero.c
@@ -0,0 +1,16 @@
+
+
+int main(){
+    int numero1;
+    int numero2;
+    char operador[4];
+    
+    printf("Insira o primeiro numero\n");
+    scanf("%d", &numero1);
+    printf("Insira o segundo numero\n");
+    scanf("%d", &numero2);
+
+    
+
+
+}
---
+++ ordernar_numero.js
@@ -0,0 +1,5 @@
+let numeros = [10, 78, 45, 69, 35, 15, 24, 66, 87];
+
+let numerosOrdenadosDecrescente = numeros.sort((a, b) => b - a);
+
+console.log("Números ordenados (decrescente):", numerosOrdenadosDecrescente);
