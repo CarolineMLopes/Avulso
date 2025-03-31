e7a6a4c93474ed3431e15bb1865496c8adf11b96
Caroline Marcon Lopes <carolinemarconlopes@gmail.com>
2025-03-31 17:01:39 -0300

    faculdade

---
+++ linguagemC/swith/exercicio2.c
@@ -0,0 +1,17 @@
+/**
+ * o pc deve escolher de 1 a 3
+ * 
+ * 1- pedra
+ * 2- papel
+ * 3- tesoura
+ * (randon)
+ * 
+ * voce escolhe tambem entre
+ * 
+ * 1- pedra
+ * 2- papel
+ * 3- tesoura
+ * (swith case)
+ * 
+ * mostrar o vencedor
+*/
